2b82a57e45392941fbc48dfbe9457628026738ce
shifa1 <ingaizoza>
2017-11-21 14:43:39 +0100

    graphs

--- app/src/main/java/com/example/daniels/myapplication/MainActivity.java
+++ app/src/main/java/com/example/daniels/myapplication/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, Graph.class);
                 startActivity(i);
+
             }
         });
     }

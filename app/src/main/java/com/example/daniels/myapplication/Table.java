ed54ec31014890e00b3d5990d8bd21cd817df176
Daniels Ricards Briedis <253965@VIAUC.DK>
2017-11-24 13:37:30 +0100

    Table work

--- app/src/main/java/com/example/daniels/myapplication/Table.java
+++ app/src/main/java/com/example/daniels/myapplication/Table.java
@@ -2,12 +2,71 @@ package com.example.daniels.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Table extends AppCompatActivity {
+
+    /*TextView bitcoin;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table);
+
+        /*          AN ATTEMPT TO SIMULATE PRICE CHANGES
+        try
+        {
+            converter();
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        */
     }
+
+    /*
+    private void converter() throws InterruptedException
+    {
+
+        String value;
+        while(true)
+        {
+            Thread.sleep(15000);
+            bitcoin = (TextView) findViewById(R.id.bitcoinValue);
+            value = (String) bitcoin.getText();
+            int result = 0;
+            int coinValue = 0;
+            int newValue = 0;
+            try
+            {
+                coinValue = Integer.parseInt(value);
+            }
+            catch (NumberFormatException e)
+            {
+
+            }
+            result = (coinValue * 5) / 100;
+            newValue = coinValue + result;
+            bitcoin.setText(newValue);
+
+            Thread.sleep(20000);
+            value = (String) bitcoin.getText();
+            result = 0;
+            coinValue = 0;
+            newValue = 0;
+            try
+            {
+                coinValue = Integer.parseInt(value);
+            }
+            catch(NumberFormatException e)
+            {
+
+            }
+            result = (coinValue * 2) / 100;
+            newValue = coinValue - result;
+            bitcoin.setText(newValue);
+        }
+    }
+    */
 }
